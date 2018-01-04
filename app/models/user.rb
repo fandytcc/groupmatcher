7bc21d584cda6e39a7cd368ad3bf27b99105d1af
fandytcc <fandytsui@gmail.com>
2018-01-04 10:46:02 +0100

    add user-to-profile association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile
+
+  def full_name
+    return profile.full_name if profile?
+
+    email
+  end
+
+  def profile?
+    profile.present? && profile.persisted?
+  end
 end

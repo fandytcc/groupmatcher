a40ba57c2d8ea6e4d4abc21c37cf05005740faca
fandytcc <fandytsui@gmail.com>
2018-01-05 13:56:49 +0100

    update narbar & filter match page for students

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,12 +2,12 @@ class MatchesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @matches = Match.all
+    @matches = Match.all.order(:id)
     @match = Match.new
   end
 
   def create
-    day = params[:match][:day] unless params[:match].nil?
+    day = params[:match][:day]
     match_today = Match.select { |match| match.day == day.to_date }
 
     if match_today.blank?
@@ -16,13 +16,13 @@ class MatchesController < ApplicationController
     redirect_to action: 'index'
   end
 
-  def show
-    @match = Match.find(params[:id])
-  end
+  # def show
+  #   @match = Match.find(params[:id])
+  # end
 
   def generate_groups(day)
-    @schedule = Group.new.days
-    @schedule.each do |day, match|
+    schedule = Group.new.days
+    schedule.each do |day, match|
       Match.create!(day: Date.today + day-1, matches: match)
     end
     # redirect_to matches_path
